23a79f39c05b2fba786d6eb924696f34a526d2ac
justinhchae <justin.chae@nlaw.northwestern.edu>
2021-04-07 21:03:35 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='pd_helper',
-    version='0.0.5',
+    version='0.0.6',
     author='Justin Chae',
     author_email='justin@chaemail.com',
     description = 'A helpful script to optimize a Pandas DataFrame.',
