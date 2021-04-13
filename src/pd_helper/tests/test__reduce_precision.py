5e04a94e5eb0eace0b5220ddd7dce55c3c26b9b1
justinhchae <justin.chae@nlaw.northwestern.edu>
2021-04-13 14:34:38 -0400

    update unittests

--- src/pd_helper/tests/test__reduce_precision.py
+++ src/pd_helper/tests/test__reduce_precision.py
@@ -3,88 +3,92 @@ from unittest import TestCase
 import pandas as pd
 
 class Test(TestCase):
+    from src.pd_helper.maker import MakeData
+    from src.pd_helper.utils._reduce_precision import _reduce_precision
+    faker = MakeData()
+    df = faker.make_df()
 
-    def test__reduce_precision(self):
-        from src.pd_helper.maker import MakeData
-        from src.pd_helper.utils._reduce_precision import _reduce_precision
-        faker = MakeData()
-        df = faker.make_df()
-        df_cols = list(df.columns)
+    # function params passed by parent
+    date_strings = ['_date', 'date_']
+    bool_types = [True, False, 'true', 'True', 'False', 'false']
+    categorical_ratio = .1
+    categorical_threshold = 20
+    final_default_dtype = 'string'
+    enable_mp = False
 
-        # function params passed by parent
-        date_strings = ['_date', 'date_']
-        bool_types = [True, False, 'true', 'True', 'False', 'false']
-        categorical_ratio = .1
-        categorical_threshold = 20
-        final_default_dtype = 'string'
-        enable_mp = False
+    def test__reduce_precision_string(self):
 
-        s = _reduce_precision(df["Object ID"]
-                              , date_strings=date_strings
-                              , bool_types=bool_types
-                              , categorical_ratio=categorical_ratio
-                              , categorical_threshold=categorical_threshold
-                              , final_default_dtype=final_default_dtype
-                              , enable_mp=enable_mp
+        s = Test._reduce_precision(Test.df["Object ID"]
+                              , date_strings=Test.date_strings
+                              , bool_types=Test.bool_types
+                              , categorical_ratio=Test.categorical_ratio
+                              , categorical_threshold=Test.categorical_threshold
+                              , final_default_dtype=Test.final_default_dtype
+                              , enable_mp=Test.enable_mp
                               )
         data_type = str(s.dtype)
-        assert data_type == 'string'
+        self.assertEqual(data_type, 'string')
 
-        s = _reduce_precision(df['Item Name']
-                              , date_strings=date_strings
-                              , bool_types=bool_types
-                              , categorical_ratio=categorical_ratio
-                              , categorical_threshold=categorical_threshold
-                              , final_default_dtype=final_default_dtype
-                              , enable_mp=enable_mp
+    def test__reduce_precision_category(self):
+        s = Test._reduce_precision(Test.df['Item Name']
+                              , date_strings=Test.date_strings
+                              , bool_types=Test.bool_types
+                              , categorical_ratio=Test.categorical_ratio
+                              , categorical_threshold=Test.categorical_threshold
+                              , final_default_dtype=Test.final_default_dtype
+                              , enable_mp=Test.enable_mp
                               )
         data_type = str(s.dtype)
-        assert data_type == 'category'
+        self.assertEqual(data_type, 'category')
 
-        s = _reduce_precision(df["Retrieved Date"]
-                              , date_strings=date_strings
-                              , bool_types=bool_types
-                              , categorical_ratio=categorical_ratio
-                              , categorical_threshold=categorical_threshold
-                              , final_default_dtype=final_default_dtype
-                              , enable_mp=enable_mp
+    def test__reduce_precision_datetime(self):
+        s = Test._reduce_precision(Test.df["Retrieved Date"]
+                              , date_strings=Test.date_strings
+                              , bool_types=Test.bool_types
+                              , categorical_ratio=Test.categorical_ratio
+                              , categorical_threshold=Test.categorical_threshold
+                              , final_default_dtype=Test.final_default_dtype
+                              , enable_mp=Test.enable_mp
                               )
 
         data_type = str(s.dtype)
-        assert data_type == 'datetime64[ns]'
+        self.assertEqual(data_type, 'datetime64[ns]')
 
-        s = _reduce_precision(df['Retrieved']
-                              , date_strings=date_strings
-                              , bool_types=bool_types
-                              , categorical_ratio=categorical_ratio
-                              , categorical_threshold=categorical_threshold
-                              , final_default_dtype=final_default_dtype
-                              , enable_mp=enable_mp
+    def test__reduce_precision_boolean(self):
+        s = Test._reduce_precision(Test.df['Retrieved']
+                              , date_strings=Test.date_strings
+                              , bool_types=Test.bool_types
+                              , categorical_ratio=Test.categorical_ratio
+                              , categorical_threshold=Test.categorical_threshold
+                              , final_default_dtype=Test.final_default_dtype
+                              , enable_mp=Test.enable_mp
                               )
         data_type = str(s.dtype)
-        assert data_type == 'boolean'
+        self.assertEqual(data_type, 'boolean')
 
-        s = _reduce_precision(df["Sector"]
-                              , date_strings=date_strings
-                              , bool_types=bool_types
-                              , categorical_ratio=categorical_ratio
-                              , categorical_threshold=categorical_threshold
-                              , final_default_dtype=final_default_dtype
-                              , enable_mp=enable_mp
+    def test__reduce_precision_int8(self):
+        s = Test._reduce_precision(Test.df["Sector"]
+                              , date_strings=Test.date_strings
+                              , bool_types=Test.bool_types
+                              , categorical_ratio=Test.categorical_ratio
+                              , categorical_threshold=Test.categorical_threshold
+                              , final_default_dtype=Test.final_default_dtype
+                              , enable_mp=Test.enable_mp
                               )
         data_type = str(s.dtype)
-        assert data_type == 'int8'
+        self.assertEqual(data_type, 'int8')
 
-        s = _reduce_precision(df["Weight"]
-                              , date_strings=date_strings
-                              , bool_types=bool_types
-                              , categorical_ratio=categorical_ratio
-                              , categorical_threshold=categorical_threshold
-                              , final_default_dtype=final_default_dtype
-                              , enable_mp=enable_mp
+    def test__reduce_precision_float16(self):
+        s = Test._reduce_precision(Test.df["Weight"]
+                              , date_strings=Test.date_strings
+                              , bool_types=Test.bool_types
+                              , categorical_ratio=Test.categorical_ratio
+                              , categorical_threshold=Test.categorical_threshold
+                              , final_default_dtype=Test.final_default_dtype
+                              , enable_mp=Test.enable_mp
                               )
         data_type = str(s.dtype)
-        assert data_type == 'float16'
+        self.assertEqual(data_type, 'float16')
 
 
 
